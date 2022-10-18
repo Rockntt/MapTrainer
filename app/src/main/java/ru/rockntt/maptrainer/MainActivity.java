be00024c7d3aec7467f1ebd5a1f1460c9bf94658
Rockntt <klochkovmv2007@gmail.com>
2022-10-18 20:39:13 +0300

    Initial commit

---
+++ app/src/main/java/ru/rockntt/maptrainer/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.rockntt.maptrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
